53da2c15a39ae4996c27981e120ee2851fa6601a
JensLundsgaard <jenslundsgaard7@gmail.com>
2023-11-04 15:18:29 -0500

    Minor changes to splash activity

--- app/src/main/java/com/example/carbongo/SplashActivity.java
+++ app/src/main/java/com/example/carbongo/SplashActivity.java
@@ -18,6 +18,6 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 startActivity(new Intent(SplashActivity.this, LoginActivity.class));
             }
-        }, 3000);
+        }, 10000);
     }
 }
