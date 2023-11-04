acb3d6b4c664d7e8dc02e2ab9c27672a3627db9d
JensLundsgaard <jenslundsgaard7@gmail.com>
2023-11-04 14:21:52 -0500

    minor change

--- app/src/main/java/com/example/carbongo/SellActivity.java
+++ app/src/main/java/com/example/carbongo/SellActivity.java
@@ -9,7 +9,7 @@ public class SellActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.activity_sell);
     }
 }
