c41bd41d567e130ee9e7307d400b008a42b955a2
rakeshvel <rakeshvel@hotmail.com>
2023-11-04 20:05:40 -0500

    Info screen

--- app/src/main/java/com/example/carbongo/InfoActivity.java
+++ app/src/main/java/com/example/carbongo/InfoActivity.java
@@ -2,7 +2,9 @@ package com.example.carbongo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(InfoActivity.this, PurchaseActivity.class));
+            }
+        }, 10000);
     }
 }
